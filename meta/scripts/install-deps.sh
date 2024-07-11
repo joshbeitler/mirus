f289f8a3bd49014f9805897317df31b5be65d15c
Josh Beitler <hello@btlr.xyz>
2024-07-10 22:44:25 -0600

    meta: Limine config

--- meta/scripts/install-deps.sh
+++ meta/scripts/install-deps.sh
@@ -1,11 +1,2 @@
-# TODO: would be nice to eventually switch to clang, llmv, lld, etc.
-brew install bison flex gmp libmpc mpfr texinfo isl libiconv xorriso
+brew install bison flex gmp libmpc mpfr texinfo isl libiconv xorriso qemu nasm
 brew install --cask gdisk
-
-
-# set -gx LDFLAGS "-L/opt/homebrew/opt/bison/lib"
-#  set -gx LDFLAGS "-L/opt/homebrew/opt/flex/lib"
- # set -gx CPPFLAGS "-I/opt/homebrew/opt/flex/include"
- # set -gx LDFLAGS "-L/opt/homebrew/opt/libiconv/lib"
- # set -gx CPPFLAGS "-I/opt/homebrew/opt/libiconv/include"
-   # set -gx LDFLAGS "-L/opt/homebrew/opt/bison/lib"
