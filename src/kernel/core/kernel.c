15a99d95d5f67c567bb360437262605813366fa8
Josh Beitler <hello@btlr.xyz>
2024-07-25 22:37:28 -0600

    kernel: Start on terminal driver

--- src/kernel/core/kernel.c
+++ src/kernel/core/kernel.c
@@ -7,38 +7,9 @@
 #include <limine.h>
 #include <ssfn.h>
 
+#include <bootloader.h>
 #include <string.h>
 #include <terminal.h>
-
-// Set the base revision to 2, this is recommended as this is the latest
-// base revision described by the Limine boot protocol specification.
-// See specification for further info.
-__attribute__((used, section(".requests")))
-static volatile LIMINE_BASE_REVISION(2);
-
-// The Limine requests can be placed anywhere, but it is important that
-// the compiler does not optimise them away, so, usually, they should
-// be made volatile or equivalent, _and_ they should be accessed at least
-// once or marked as used with the "used" attribute as done here.
-__attribute__((used, section(".requests")))
-static volatile struct limine_framebuffer_request framebuffer_request = {
-  .id = LIMINE_FRAMEBUFFER_REQUEST,
-  .revision = 0
-};
-
-__attribute__((used, section(".requests")))
-static volatile struct limine_module_request module_request = {
-    .id = LIMINE_MODULE_REQUEST,
-    .revision = 0
-};
-
-// Finally, define the start and end markers for the Limine requests.
-// These can also be moved anywhere, to any .c file, as seen fit.
-__attribute__((used, section(".requests_start_marker")))
-static volatile LIMINE_REQUESTS_START_MARKER;
-
-__attribute__((used, section(".requests_end_marker")))
-static volatile LIMINE_REQUESTS_END_MARKER;
 
 struct limine_file *getFile(const char *name) {
   struct limine_module_response *module_response = module_request.response;
@@ -61,12 +32,6 @@ static void hcf(void) {
     asm ("hlt");
   }
 }
-
-// void ssfn_puts(char* str) {
-//   for (size_t i = 0; i < strlen(str); i++) {
-//     ssfn_putc(str[i]);
-//   }
-// }
 
 void _start(void) {
   // Ensure the bootloader actually understands our base revision (see spec).
