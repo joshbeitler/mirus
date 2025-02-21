a0d2f3b4ec82ab909879a17f40ba67e2e52de01d
Josh Beitler <hello@btlr.xyz>
2025-02-21 10:54:45 -0700

    meta: Checkin

--- src/kernel/memory/vmm.c
+++ src/kernel/memory/vmm.c
@@ -5,8 +5,39 @@
 #include <kernel/memory/pmm.h>
 #include <kernel/debug.h>
 
-// Note for tomorrow self: as we get into this, a proper unit testing framework
-// (unity maybe?) would be nice to ensure things are working as expected, and
-// are repeatable
+void vmm_initialize() {
+    // TOOD: what is there to initialize here
+    // paging is set up already
+    // hhdm is set up already
+}
 
-// How necessary is the VMM if we intend to operate a kernel-level VM?
+void vmm_map_page(uintptr_t virt, uintptr_t phys, int flags) {
+
+}
+
+void vmm_unmap_page(uintptr_t virt) {
+
+}
+
+void vmm_set_page_flags(uintptr_t virt, int flags) {
+
+}
+
+int vmm_get_page_flags(uintptr_t virt) {
+}
+
+void vmm_create_address_space() {
+
+}
+
+void vmm_switch_address_space() {
+
+}
+
+void vmm_destroy_address_space() {
+
+}
+
+void vmm_clone_address_space() {
+
+}
